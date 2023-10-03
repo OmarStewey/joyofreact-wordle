948fa9937c72f6ad51791504cefd494ce8811e08
Omar Stewey <OmarStewey@Outlook.com>
2023-10-03 12:12:27 -0400

    init

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,13 +3,53 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
+import GuessingBox from '../GuessingBox';
+import GuessResults from '../GuessResults';
+
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import Banner from '../Banner/Banner';
+import { checkGuess } from '../../game-helpers';
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+
+  const [guesses, setGuesses] = React.useState([]);
+  const [gameOver, setGameOver] = React.useState(false);
+  const [gameStatus, setGameStatus] = React.useState('');
+
+  function addGuess(guess) {
+    let nextGuesses = [...guesses];
+    nextGuesses.push(guess);
+    setGuesses(nextGuesses);
+
+
+    if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameOver(true);
+      setGameStatus("lose");
+    } else {
+      if (checkGuess(guess, answer).filter(({ letter, status }) => {
+        return status === "correct";
+      }).length === 5) {
+        setGameStatus("win");
+        setGameOver(true);
+      }
+    }
+
+  }
+
+  return (
+    <>
+      {
+        (gameOver && <Banner status={gameStatus} answer={answer} attempts={guesses.length} />)
+      }
+      <GuessResults guesses={guesses} answer={answer} />
+      { !gameOver && <GuessingBox addGuess={addGuess} /> }
+    </>
+  )
 }
 
 export default Game;
